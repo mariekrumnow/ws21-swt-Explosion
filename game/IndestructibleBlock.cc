f393c67f011e5e7b33e24ae1e965d71a3d39296a
mariekrumnow <93008705+mariekrumnow@users.noreply.github.com>
2021-12-03 10:22:42 +0100

    Reihenfolge von SetPosition & AddObject getauscht

---
+++ game/IndestructibleBlock.cc
@@ -0,0 +1,43 @@
+//
+// Autor: Nina, Marie
+//
+
+#include "Block.h"
+#include "IndestructibleBlock.h"
+#include "../graphics/Tile.h"
+#include "../graphics/Color.h"
+
+namespace game {
+
+namespace obstacles{
+
+    IndestructibleBlock::IndestructibleBlock() : Block() {}
+
+    /// If a nullptr is returned, an error occured or the object couldn't be placed
+    IndestructibleBlock* IndestructibleBlock::CreateIndestructibleBlock(int x, int y){
+          IndestructibleBlock* temp = new IndestructibleBlock();
+          if (temp!=nullptr){
+                GameManager::GetCurrentGame().AddGameObject(*temp);
+                if (!temp->SetPosition(x,y)) {
+                      temp->Destroy();
+                      return nullptr;
+                }
+          }
+          return temp;
+    }
+
+    bool IndestructibleBlock::OnExplosion(GameObject& source) {
+          return true;
+    }
+
+    graphics::Tile IndestructibleBlock::GetTile() {
+          return graphics::kTileSolidWall;
+    }
+
+    graphics::Color IndestructibleBlock::GetColor() {
+          return graphics::Color(105, 105, 105, 255);
+    }
+
+} // namespace obstacles
+
+} // namespace game
---
+++ game/SpeedUpgrade.cc
@@ -0,0 +1,51 @@
+//Autor Peter, Patrick, Marie
+
+#include "SpeedUpgrade.h"
+
+#include "../GameObject.h"
+#include "../GameManager.h"
+#include "../Player.h"
+#include "../../core/AppManager.h"
+#include "../../graphics/Color.h"
+#include "../../graphics/Tile.h"
+
+namespace game{
+    namespace upgrade{
+        SpeedUpgrade::SpeedUpgrade() : Upgrade() {}
+
+        /// If a nullptr is returned, an error occured or the object couldn't be placed
+        SpeedUpgrade* SpeedUpgrade::CreateSpeedUpgrade(int x, int y){
+              SpeedUpgrade* temp = new SpeedUpgrade();
+              if (temp!=nullptr){
+                    GameManager::GetCurrentGame().AddGameObject(*temp);
+                      if (!temp->SetPosition(x,y)) {
+                            temp->Destroy();
+                            return nullptr;
+                      }
+              }
+              return temp;
+        }
+
+        bool SpeedUpgrade::OnPlayerCollision(Player& player) {
+            this->Destroy();
+            if(player.GetSpeed() < player.GetKMaxSpeed()){
+                player.IncreaseSpeed(1);
+            }
+
+            return true;
+        }
+
+        bool SpeedUpgrade::OnExplosion(GameObject& source){
+            this->Destroy();
+            return false;
+        }
+
+        graphics::Color SpeedUpgrade::GetColor(){
+            return graphics::Color(255,255,0,255);
+        }
+
+        graphics::Tile SpeedUpgrade::GetTile(){
+            return graphics::kTileSpeedUpgrade;
+        }
+    }
+}
